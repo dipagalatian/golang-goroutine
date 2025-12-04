2ae276ce9cd805c922e2aa945978dcf8b34cf986
dipagalatian <dipagalatian3003@gmail.com>
2025-12-04 21:13:46 +0700

    FEAT: Create goroutine with keyword go

---
+++ goroutine_test.go
@@ -0,0 +1,24 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func HelloWorld() {
+	fmt.Println("Hello World")
+}
+
+
+// To run func with goroutines, we can add keyword "go" before call a func
+// This execution of TestCreateGoroutines will run asynchronous
+// If the goroutines still not finish yet, it will not wait for the goroutines to finish
+// Instead it will continue execution the next logic
+// time.Sleep() used for make sure goroutines have enough time to run before the func stop the execution
+func TestCreateGoroutine(t *testing.T) {
+	go HelloWorld()
+	fmt.Println("Goroutine Test Done")
+
+	time.Sleep(1 * time.Second)
+}
