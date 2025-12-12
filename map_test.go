670a552f36518c8ec78723b44b822701202f3e9e
dipagalatian <dipagalatian3003@gmail.com>
2025-12-12 16:07:10 +0700

    FEAT: Implement sync.Map

---
+++ map_test.go
@@ -0,0 +1,39 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func AddToMap(data *sync.Map, value int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	data.Store(value, value)
+	
+}
+
+func TestMap(t *testing.T) {
+
+	data := &sync.Map{}
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+
+		wg.Add(1)
+		go AddToMap(data, i, wg)
+
+	}
+
+	wg.Wait()
+	fmt.Println("Goroutines finish")
+
+	data.Range(func (key, value any) bool  {
+
+		fmt.Println(key, ":", value)
+
+		return true
+		
+	})
+	
+}
